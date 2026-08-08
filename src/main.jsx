898112772d25853bfcb57f7c617827a282510ddb
Abdus Salam <etechtalk87@gmail.com>
2026-08-08 13:13:59 +0400

    fix InvoiceDetail.jsx missing company_id causing blank company info on invoice PDFs; add Buffer polyfill for react-pdf image rendering in browser

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Buffer } from 'buffer'
 import './index.css'
 import './lib/i18n.js'
 import App from './App.jsx'
 import { AuthProvider } from './lib/AuthContext.jsx'
+
+window.Buffer = window.Buffer || Buffer
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
